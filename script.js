6c0fb17c2d73837fe152916372db0ce61d40a025
Emma30h <emma30h@gmail.com>
2022-05-24 22:31:19 -0300

    Subimos el repositorio

---
+++ script.js
@@ -0,0 +1,28 @@
+const input = document.querySelector("#searchInput");
+const userList = document.querySelector("#users");
+
+let users = [];
+
+window.addEventListener("DOMContentLoaded", async()=>{
+    userList.innerHTML = `<h1>Loading...</h1>`;
+    const data = await loadUsers();
+    users = data.data;
+    renderUsers(users);
+})
+
+async function loadUsers(){
+  const response = await fetch(`https://fakerapi.it/api/v1/users?_quantity=1000`)
+  return await response.json();
+}
+
+const createUserItems = users => users.map(user => `<li>${user.firstname} ${user.lastname}</li>`).join(` `)
+
+function renderUsers(users){
+    const itemsString = createUserItems(users);
+    userList.innerHTML = itemsString;
+}
+
+input.addEventListener("keyup", e=>{
+    const newUsers = users.filter(user => `${user.firstname.toLowerCase()} ${user.lastname.toLowerCase()}`.includes(input.value.toLowerCase()));
+    renderUsers(newUsers);
+})
